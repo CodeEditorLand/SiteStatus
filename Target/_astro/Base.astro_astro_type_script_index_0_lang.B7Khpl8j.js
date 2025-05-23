d0ffc7b52c3b08b42c97165938a8a1050c70bfdf
Node <Node@PlayForm.Cloud>
2025-05-23 03:18:02 +0000

    Node: Build artifacts (Node 20) [skip-node-commit]

---
+++ Target/_astro/Base.astro_astro_type_script_index_0_lang.B7Khpl8j.js
@@ -0,0 +1,16 @@
+import { _ as a } from "./preload-helper.Cat91CNq.js";
+(
+	await a(async () => {
+		const { initializeApp: a } = await import("./index.esm.BulNnHcP.js");
+		return { initializeApp: a };
+	}, [])
+).initializeApp({
+	apiKey: "",
+	appId: "",
+	authDomain: "",
+	databaseURL: "",
+	measurementId: "",
+	messagingSenderId: "",
+	projectId: "",
+	storageBucket: "",
+});
