581584173cb58f67aa94b27778486d95c6ecb1cb
Node <Node@PlayForm.Cloud>
2025-05-24 05:49:02 +0000

    [Skip=Node]

--- Target/_astro/Base.astro_astro_type_script_index_0_lang.D3wwwDfJ.js
+++ Target/_astro/Base.astro_astro_type_script_index_0_lang.D3wwwDfJ.js
@@ -1,7 +1,7 @@
 import { _ as a } from "./preload-helper.Cat91CNq.js";
 (
 	await a(async () => {
-		const { initializeApp: a } = await import("./index.esm.BulNnHcP.js");
+		const { initializeApp: a } = await import("./index.esm.BHEwOI5X.js");
 		return { initializeApp: a };
 	}, [])
 ).initializeApp({
