47041d35f5cf58351d3d6aaed22e020a97f6f269
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-05-05 12:39:09 +0300

    refactor(cache): delete orphaned Get/Key.ts

--- Source/Function/Table/Layout/Request/Get/Key.ts
+++
@@ -1,7 +0,0 @@
-import { createHash } from "node:crypto";
-
-import type { PARAMETER } from "../Octokit.js";
-
-/** Return the MD5 cache key for a given request without touching the filesystem. */
-export const Key = (parameter: PARAMETER): string =>
-	createHash("md5").update(JSON.stringify(parameter)).digest("hex");
